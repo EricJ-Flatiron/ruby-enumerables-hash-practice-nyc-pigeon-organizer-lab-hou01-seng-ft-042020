e116e6b1182eaf8e77023bd8a1e226e86f4c6582
Eric Johnson <johnson.n.eric@gmail.com>
2020-04-16 23:52:54 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -3,15 +3,15 @@ require 'pry'
 def nyc_pigeon_organizer(data)
   # write your code here!
   
-  array_of_names = []
-  data[:color].each do |color|
-    color.each do
-      array_of_names << color
-    end
-  end
+  # array_of_names = []
+  # data[:color].each do |color|
+  #   color.each do
+  #     array_of_names << color
+  #   end
+  # end
   
   
-  
+  data.first
 
 
 
